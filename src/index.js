83fdbe2f7f8b2edad17689ddb6e25cb7d1d21336
Zainab-Alzubair <zainab.a.elzubair@gmail.com>
2022-11-22 02:29:44 +0300

    Change varibale name

--- src/index.js
+++ src/index.js
@@ -4,19 +4,18 @@ import getData from './moduled/getData.js';
 // Get All meals Data
 getData()
   .then((data) => {
-    let result = '';
+    let mealCard = '';
     data.meals.forEach((elem) => {
-      result += `  
+      mealCard += `  
     <div class="card">
     <img src="${elem.strMealThumb}" alt="Sample photo">
     <div class="text">
       <h3>${elem.strMeal} ❤</h3>
       <h5 class="likes">2 likes</h5>
-      
       <button>Comment</button>
     </div>
     </div>
 `;
     });
-    document.querySelector('.grid').innerHTML = result;
+    document.querySelector('.grid').innerHTML = mealCard;
   });
