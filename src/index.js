39c656e3785b79ef63691bd32204adec71dcd065
Zainab-Alzubair <zainab.a.elzubair@gmail.com>
2022-11-22 01:48:45 +0300

    Fetch API data, Create page structure

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
-import _ from "lodash";
-import "./style.css";
+import './style.css';
+import getData from './moduled/getData.js';
 
-function component() {
-  const element = document.createElement("div");
-
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
-  element.classList.add("hello");
-
-  return element;
-}
-
-document.body.appendChild(component());
+// Get All meals Data
+getData()
+  .then((data) => {
+    let result = '';
+    data.meals.forEach((elem) => {
+      result += `  
+    <div class="card">
+    <img src="${elem.strMealThumb}" alt="Sample photo">
+    <div class="text">
+      <h3>${elem.strMeal} ❤</h3>
+      <h5 class="likes">2 likes</h5>
+      
+      <button>Comment</button>
+    </div>
+    </div>
+`;
+    });
+    document.querySelector('.grid').innerHTML = result;
+  });
