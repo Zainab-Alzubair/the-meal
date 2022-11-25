5082443150900f7a105eb864a5fc7098fc5f141f
Zainab-Alzubair <zainab.a.elzubair@gmail.com>
2022-11-25 17:40:26 +0300

    Add likes functions

--- src/index.js
+++ src/index.js
@@ -1,20 +1,45 @@
+import _ from 'lodash';
 import "./style.css";
-import getData from "./modules/getData.js";
-
+import {getData} from "./modules/getData.js";
+import {getLikes} from "./modules/getData.js";
+import { addLike } from './modules/mealsLikes.js';
 // Get All meals Data
-getData().then((data) => {
-  let mealCard = "";
-  data.meals.forEach((elem) => {
-    mealCard += `  
-    <div class="card">
+getData()
+  .then((data) => {
+    let mealCard = '';
+    data.meals.forEach((elem) => {
+      mealCard += `  
+      <div id="${elem.idMeal}" class="card">
     <img src="${elem.strMealThumb}" alt="Sample photo">
     <div class="text">
-      <h3>${elem.strMeal} ❤</h3>
-      <h5 class="likes">2 likes</h5>
-      <button>Comment</button>
+      <h3>${elem.strMeal} </h3>
+      <h4 class="likes-number">No likes</h4>
+      <button class="like-heart">Like 👍</button>
+      <button>Comment 💬</button>
     </div>
     </div>
 `;
-  });
-  document.querySelector(".grid").innerHTML = mealCard;
+//display likes number in homepage
+getLikes().then((data) => {
+  const likes =document.querySelectorAll('.likes-number')
+  data.filter((item) => {
+      likes.forEach((like)=>{
+        let cardId = like.parentElement.parentElement.id
+        if (item.item_id === cardId) {
+          like.innerHTML = `${item.likes} likes`;
+      }
+      })
+    });
+//get likes ID when like button clicked
+  const likeBtn = document.querySelectorAll('.like-heart')
+  for(let i=0;i<likeBtn.length;i++){
+    likeBtn[i].onclick = () => {
+      const {id } = likeBtn[i].parentElement.parentElement;
+      addLike(id, likes);
+    };
+  }
+ 
+});
+  document.querySelector('.grid').innerHTML = mealCard;
+})
 });
--- src/modules/getData.js
+++ src/modules/getData.js
@@ -1,7 +1,27 @@
-const getData = async () => {
-  const response = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?f=v');
+//Get all meals data from API
+export const getData = async () => {
+  try {
+  const response = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?f=b');
   const meals = await response.json();
   return meals;
+} catch (e) {
+  return e.message;
+}
 };
 
-export default getData;
+//Set counter for all meals in navbar
+getData().then((data) => {
+  let count = 0
+  const mealsCount = document.getElementById('meals-count')
+    for(let i =0; i<data.length;i+=1){
+      count+=1
+    }
+    mealsCount.innerHTML=`Meals  `+'('+count+')'
+  })
+//get all likes from API
+export const getLikes = async () => {
+    const response = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/e74v3gv4fqA3Iq80tG1r/likes');
+    const data = await response.json();
+    return data
+  };
+  
