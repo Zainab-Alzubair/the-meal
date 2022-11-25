6cd6e7ae91aa07389b4fbebfeeb17f1e8c049fae
z4cope <63551733+z4cope@users.noreply.github.com>
2022-11-25 22:56:35 +0200

    Fix linters errors

--- src/modules/renderCommentsList.js
+++ src/modules/renderCommentsList.js
@@ -1,13 +1,13 @@
-import { getComments } from "../apis/requests.js";
+import { getComments } from '../apis/requests.js';
 
 class RenderComments {
   static renderCommentsList = async (cardId) => {
     // Get method to view the comments
     const comments = await getComments(cardId);
-    const commentsList = document.querySelector(".comment-section");
+    const commentsList = document.querySelector('.comment-section');
     this.commentsCount(comments);
 
-    commentsList.innerHTML = ""; /* eslint-disable */
+    commentsList.innerHTML = ''; /* eslint-disable */
     comments.length
       ? comments.forEach((comment) => {
           commentsList.innerHTML += `
--- src/modules/test/commentsCounterTest.test.js
+++ src/modules/test/commentsCounterTest.test.js
@@ -1,16 +1,15 @@
-import Render from "../renderCommentsList";
+import Render from '../renderCommentsList.js';
 
-describe("Count test", () => {
-  test("should count comments number", () => {
-    document.body.innerHTML =
-      "<h3>Comments (<span class='comments-count'></span>)</h3>";
+describe('Count test', () => {
+  test('should count comments number', () => {
+    document.body.innerHTML = "<h3>Comments (<span class='comments-count'></span>)</h3>";
     const array = [
-      { creation_date: "2022-11-24", username: "zenib", comment: "good" },
-      { creation_date: "2022-11-24", username: "batrek", comment: "bad" },
+      { creation_date: '2022-11-24', username: 'zenib', comment: 'good' },
+      { creation_date: '2022-11-24', username: 'batrek', comment: 'bad' },
     ];
 
     Render.commentsCount(array);
-    const comment = document.querySelector(".comments-count").textContent;
+    const comment = document.querySelector('.comments-count').textContent;
 
     expect(Number(comment)).toEqual(2);
   });
